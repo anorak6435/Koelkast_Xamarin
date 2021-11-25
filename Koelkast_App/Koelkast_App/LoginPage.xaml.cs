245a93b074268472f31bcbbd4f0f3c454243f498
anorak6435 <stefan.beckers14@gmail.com>
2021-11-25 10:58:07 +0100

    Add project files.

---
+++ Koelkast_App/Koelkast_App/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Koelkast_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnLoggingIn_Clicked(object sender, EventArgs e)
+        {
+            // TODO: check the user entered valid credentials
+            await Navigation.PushAsync(new UserBalancePage());
+        }
+    }
+}
